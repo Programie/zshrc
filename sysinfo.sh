370ef18e204c30f3c8f2a63ba245726112a401df
Michael Wieland <github@selfcoders.com>
2014-10-23 19:09:19 +0200

    Fixed empty sysinfo output on Mac OS X Added variables to disable some output

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,45 +1,25 @@
+STRING=""
+
 which uptime > /dev/null 2>&1
 if [ "$?" = "0" ]; then
-	UPTIME_DAYS="`uptime | sed -nE 's/.*\s+up ([0-9]+) day(.*)/\1/p'`" # Get number of days (Empty string if < 1 day)
-	UPTIME="`uptime | sed -nE 's/.*\s+up ([0-9]+) day(.*) ([0-9]+):([0-9]+)(.*)/\3:\4/p'`" # Get uptime in 'hh:mm' format (Empty string if time < 1 day)
-
-	if [ -z "$UPTIME" ]; then
-		UPTIME_MIN="`uptime | sed -nE 's/.*\s+up ([0-9]+) day(.*) ([0-9]+) min(.*)/\3/p'`" # Get uptime in 'x min' format (Empty string if time < 60 min or < 1 day)
-		if [ "$UPTIME_MIN" ]; then
-			UPTIME="$UPTIME_MIN min"
-		fi
+	if [ -z "$SYSINFO_DISABLE_UPTIME" ]; then
+		STRING="$STRING   Uptime: `uptime | sed -e 's/.*up\(.*\)user.*$/\1/' | rev | cut -d ',' -f 2- | rev | sed -e 's/^ *//' -e 's/ *$//'`"
 	fi
 
-	if [ -z "$UPTIME" ]; then
-		UPTIME="`uptime | sed -nE 's/.*\s+up([ ]+)([0-9]+):([0-9]+)(.*)/\2:\3/p'`" # Get uptime in 'hh:mm' format (Empty string if time < 60 min or >= 1 day)
+	if [ -z "$SYSINFO_DISABLE_LOAD" ]; then
+		STRING="$STRING   Load: `uptime | sed -e 's/.*load average[s]*: \(.*\)/\1/'`"
 	fi
-
-	if [ -z "$UPTIME" ]; then
-		UPTIME_MIN="`uptime | sed -nE 's/.*\s+up ([0-9]+) min(.*)/\1/p'`" # Get uptime in 'x min' format
-		if [ "$UPTIME_MIN" ]; then
-			UPTIME="$UPTIME_MIN min"
-		fi
-	fi
-
-	if [ -n "$UPTIME_DAYS" ]; then
-		if [ "$UPTIME_DAYS" = "1" ]; then
-			UPTIME="1 day, $UPTIME"
-		else
-			UPTIME="$UPTIME_DAYS days, $UPTIME"
-		fi
-	fi
-
-	LOAD="`uptime | sed -nE 's/.*\s+load average: (.*)/\1/p'`"
-else
-	UPTIME="N/A"
-	LOAD="N/A"
 fi
 
-which last > /dev/null 2>&1
-if [ "$?" = "0" ]; then
-	USERS="`last | grep 'still logged in' -c`"
-else
-	USERS="N/A"
+if [ -z "$SYSINFO_DISABLE_USERS" ]; then
+	which last > /dev/null 2>&1
+	if [ "$?" = "0" ]; then
+		STRING="$STRING   Users: `last | grep 'still logged in' -c`"
+	fi
 fi
 
-echo "\e[0mUptime: \e[1;32m$UPTIME\e[0m   Load: \e[1;32m$LOAD\e[0m   Users: \e[1;32m$USERS\e[0m   Processes: \e[1;32m`ps ax | wc -l | sed -e 's: ::g'`\e[0m"
+if [ -z "$SYSINFO_DISABLE_PROCESSES" ]; then
+	STRING="$STRING   Processes: `ps ax | wc -l | sed -e 's: ::g'`"
+fi
+
+echo $STRING | sed -e 's/^ *//' -e 's/ *$//'
